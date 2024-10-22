2a8cb009b0d74307f7b6386b5d77fbd22c2bd2db
GuriGris <149690987+GuriGris@users.noreply.github.com>
2024-10-22 13:27:42 +0200

    Fil

---
+++ System/index.js
@@ -0,0 +1,36 @@
+const checkBoks = document.querySelectorAll("span");
+var count = 0;
+
+for (var i = 0; i < checkBoks.length; i++){
+
+     for (var bever = 0; bever < 7; bever++){
+        count ++;
+        var newCheckBox = document.createElement("input");
+        newCheckBox.type = "checkbox" ;
+        newCheckBox.classList.add("fin-boks");
+        newCheckBox.name = count;
+        checkBoks[i].appendChild(newCheckBox);
+     }
+
+}
+
+function saveCheckboxState() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+        localStorage.setItem(checkbox.name, checkbox.checked);
+    });
+}
+
+function loadCheckboxState() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+        const isChecked = localStorage.getItem(checkbox.name) === 'true';
+        checkbox.checked = isChecked;
+    });
+}
+
+document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
+    checkbox.addEventListener('change', saveCheckboxState);
+});
+
+window.addEventListener('load', loadCheckboxState);
